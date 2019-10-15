9d32122b11b1038371fc1614cce6830a5f057643
Andrei Zvonimir Crnkovic <andrei@infinum.co>
2019-10-15 16:13:17 +0200

    Find

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 const accounts = [{ balance: 100.5 }, { balance: 0.5 }, { balance: -10.45 }];
 
-const negativeAccountsFor = [];
+let negativeAccount = null;
 
 for (let i = 0; i < accounts.length; i++) {
-  const element = accounts[i];
+  const account = accounts[i];
 
-  if (element.balance < 0) {
-    negativeAccountsFor.push(element);
+  if (account.balance < 0) {
+    negativeAccount = account;
+    break;
   }
 }
-
-const negativeAccounts = accounts.filter(function(account) {
-  return account.balance < 0;
-});
