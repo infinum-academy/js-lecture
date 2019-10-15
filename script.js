c975b68704e4fea3e51729726a6a75f5c4271f2a
Andrei Zvonimir Crnkovic <andrei@infinum.co>
2019-10-15 16:21:07 +0200

    find and filter with reduce

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const accounts = [{ balance: 100.5 }, { balance: 0.5 }, { balance: -10.45 }];
 
-let negativeAccount = null;
-
-for (let i = 0; i < accounts.length; i++) {
-  const account = accounts[i];
-
+const negativeAccounts = accounts.reduce(function(pV, account) {
   if (account.balance < 0) {
-    negativeAccount = account;
-    break;
+    pV.push(account);
   }
-}
+
+  return pV;
+}, []);
+
+const negativeAccount = accounts.reduce(function(pV, account) {
+  if (account.balance < 0) {
+    pV = account;
+  }
+}, undefined);
