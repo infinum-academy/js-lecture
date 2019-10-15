6751a692480e705e3196b796b40d74e8f8c90b20
Andrei Zvonimir Crnkovic <andrei@infinum.co>
2019-10-15 16:10:16 +0200

    Filter

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
-const array = [
-  {
-    firstName: 'a',
-    lastName: 'b',
-  },
-  {
-    firstName: 'a',
-    lastName: 'b',
-  },
-  {
-    firstName: 'a',
-    lastName: 'b',
-  },
-];
+const accounts = [{ balance: 100.5 }, { balance: 0.5 }, { balance: -10.45 }];
 
-const completeUsers = array.map(function(user) {
-  return {
-    fullName: `${user.firstName} ${user.lastName}`,
-    ...user,
-  };
+const negativeAccountsFor = [];
+
+for (let i = 0; i < accounts.length; i++) {
+  const element = accounts[i];
+
+  if (element.balance < 0) {
+    negativeAccountsFor.push(element);
+  }
+}
+
+const negativeAccounts = accounts.filter(function(account) {
+  return account.balance < 0;
 });
