82ecee20b23653b36fad405baa4cb18d9e0e4d8c
Andrei Zvonimir Crnkovic <andrei@infinum.co>
2019-10-17 16:23:27 +0200

    Save state

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,25 @@
+import React from 'react';
+
+function Name({ name }) {
+  return <h1>{name}</h1>;
+}
+
 function Index() {
+  const [value, setValue] = React.useState('slovenija');
+
   function onButtonClick() {
-    console.log('button clicked');
+    console.log('button clicked', value);
+  }
+
+  function onInputChange(e) {
+    setValue(e.target.value);
   }
 
   return (
     <div>
+      <Name name="andrei" />
       <h1>Hello world</h1>
+      <input type="text" onChange={onInputChange} value={value} />
       <button onClick={onButtonClick}>
         Click me
       </button>
