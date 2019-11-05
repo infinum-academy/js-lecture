3d76c30ede61b6b92970b8d06f788b51f0400315
Andrei Zvonimir Crnkovic <andrei@infinum.com>
2019-11-05 16:47:45 +0100

    fill in data

--- pages/form.js
+++ pages/form.js
@@ -1,9 +1,28 @@
 import { observer } from 'mobx-react';
 import React from 'react';
 import useForm from 'react-hook-form';
+import { useAsync } from 'react-use';
+
+function fakeGetData() {
+  return new Promise((resolve) =>
+    resolve({
+      name: 'andrei',
+      email: 'andrei@infinum.co',
+      password: 'andreijenajbolji123!',
+    }),
+  );
+}
 
 function Form() {
-  const { errors, register, handleSubmit } = useForm();
+  const { value } = useAsync(fakeGetData);
+  const { errors, register, handleSubmit, setValue } = useForm();
+
+  React.useEffect(() => {
+    if (value) {
+      Object.keys(value) // ['name', 'email', 'password']
+        .forEach((key) => setValue(key, value[key]));
+    }
+  }, [value]);
 
   function onSubmit(data) {
     console.log('logging you in...', data);
@@ -20,7 +39,14 @@ function Form() {
       />
       {errors.name && <span>{errors.name.message}</span>}
       <input type="email" name="email" ref={register({})} />
-      <input type="password" name="password" ref={register({})} />
+      <input
+        type="password"
+        name="password"
+        ref={register({
+          validate: (value) => value.length >= 6 || 'Password bla',
+        })}
+      />
+      {errors.password && <span>{errors.password.message}</span>}
       <input type="submit" />
     </form>
   );
